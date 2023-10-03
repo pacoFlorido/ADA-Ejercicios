21cb0fffa60de24f8face59a77554818cf280d0a
Paco Florido <franciscoflorido0@gmail.com>
2023-10-03 15:24:26 +0200

    Ejercicios

--- src/ReadingFiles/Ejer4.java
+++ src/ReadingFiles/Ejer4.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Ejer4 {
     public static void main(String[] args) {
         Path texto = Path.of("files/texto.txt");
         Path info = Path.of("files/info_filtrado.txt");
+
         if (!Files.exists(info)){
             try {
                 Files.createFile(info);
