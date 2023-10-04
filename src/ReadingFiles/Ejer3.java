bb5507b30e0774b02483dcc8ef4e7337f5bc301a
Paco Florido <franciscoflorido0@gmail.com>
2023-10-04 22:16:29 +0200

    Ejercicio 5

--- src/ReadingFiles/Ejer3.java
+++ src/ReadingFiles/Ejer3.java
@@ -3,18 +3,28 @@ package ReadingFiles;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Ejer3 {
     public static void main(String[] args) {
         Path path = Path.of("files/numeros.txt");
 
         try {
-            String[] numeros = Files.readString(path).split(",");
-            int suma = 0;
-            for (String s: numeros){
-                suma+=Integer.parseInt(s);
+            Pattern p = Pattern.compile(",");
+            List<String> stringNums =  Files.lines(path)
+                    .flatMap(cadena -> p.splitAsStream(cadena))
+                    .toList();
+            List<Double> nums = stringNums.stream()
+                    .filter(s -> s.matches("\\d"))
+                    .map(s -> Double.parseDouble(s))
+                    .toList();
+            double suma=0;
+            for (Double i : nums){
+                suma+=i;
             }
-            System.out.println("Media: " + (suma/numeros.length));
+            System.out.printf("La media es: " + (suma/nums.size()));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
