744300a1e05bbdde9d79fc0da50302a6533613f8
Paco Florido <franciscoflorido0@gmail.com>
2023-10-14 01:31:17 +0200

    Realizando Tarea Objetos y JSON - Ejer3 a Medias

---
+++ src/ObjetosyJSON/Ejer3.java
@@ -0,0 +1,108 @@
+package ObjetosyJSON;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejer3 {
+    public static String stream(String url) {
+        try (InputStream input = new URL(url).openStream()) {
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder json = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) {
+                json.append((char) c);
+            }
+            return json.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+        String url = "https://mocki.io/v1/35c7d907-68c6-454f-9161-aa98948838d3";
+        List<Sede> sedes = new ArrayList<>();
+        List<Proyecto> proyectos = new ArrayList<>();
+        List<Evento> eventos = new ArrayList<>();
+        List<Conferencia> conferencias = new ArrayList<>();
+
+        String jsonString = stream(url);
+        JSONArray ja = new JSONArray(jsonString);
+        for (int i = 0; i<ja.length(); i++){
+            JSONObject elementoArray = ja.getJSONObject(i);
+
+            //Encontrando y asignando las Sedes
+
+            List<String> carreras = new ArrayList<>();
+            JSONObject joSede = elementoArray.getJSONObject("sede");
+            JSONArray jaCarreras = joSede.getJSONArray("carreras");
+
+            for (int j = 0; j<jaCarreras.length(); j++){
+                carreras.add(jaCarreras.getString(j));
+            }
+
+            sedes.add(new Sede(
+                    joSede.getString("nombre"),
+                    joSede.getString("ubicacion"),
+                    carreras
+            ));
+
+            //Encontrando y asignando los Proyectos
+
+            JSONArray jaProyectosTotales = elementoArray.getJSONArray("proyectos");
+            for (int k = 0; k<jaProyectosTotales.length(); k++){
+
+                List<String> equipos = new ArrayList<>();
+                JSONObject joProyecto = jaProyectosTotales.getJSONObject(k);
+                JSONArray jaEquipo = joProyecto.getJSONArray("equipo");
+                for (int l = 0; l<jaEquipo.length(); l++){
+                    equipos.add(jaEquipo.getString(l));
+                }
+                proyectos.add(new Proyecto(
+                        joProyecto.getString("nombre"),
+                        equipos,
+                        joProyecto.getBoolean("activo"),
+                        joProyecto.getInt("presupuesto")
+                ));
+            }
+
+            //Encontrando y asignando los Eventos
+
+            JSONObject joEventos = elementoArray.getJSONObject("eventos");
+            JSONArray jaConferencias = joEventos.getJSONArray("conferencias");
+
+            for (int p = 0; p<jaConferencias.length(); p++){
+
+                List<String> ponentes = new ArrayList<>();
+                JSONObject joConferencias = jaConferencias.getJSONObject(p);
+                JSONArray jaConferencia = joConferencias.getJSONArray("ponentes");
+                for (int o = 0; o<jaConferencia.length(); o++){
+                    ponentes.add(jaConferencia.getString(o));
+                }
+                conferencias.add(new Conferencia(
+                        joConferencias.getString("nombre"),
+                        ponentes,
+                        joConferencias.getString("fecha")
+                ));
+            }
+            eventos.add(new Evento(conferencias));
+        }
+
+        System.out.println(sedes);
+        System.out.println(proyectos);
+        System.out.println(eventos);
+
+
+
+
+
+
+    }
+}
